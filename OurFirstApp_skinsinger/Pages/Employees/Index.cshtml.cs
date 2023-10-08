d48bf2c3581039911f1f15517da643b4edc3ab68
kinsingersara <147285894+kinsingersara@users.noreply.github.com>
2023-10-07 20:13:32 -0400

    Add project files.

---
+++ OurFirstApp_skinsinger/Pages/Employees/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OurFirstApp_skinsinger.Pages.Employees
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ OurFirstApp_skinsinger/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OurFirstApp_skinsinger.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+       
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string? Message { get; set; }
+        public string? Learn { get; set; }
+
+        public void OnGet()
+        {
+            Message = $"Hello World {DateTime.Now.ToLongTimeString()}";
+            Learn ="I don't really feel like I know a specific thing I'd like to learn " +
+                "about Active Server Page Development, just continue to learn and become more fluent."
+        }
+    }
+}
